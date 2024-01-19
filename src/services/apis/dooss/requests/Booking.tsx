734f14c2a1d73b2843869136e8680c5ee6c2f595
Kelvin Vegilla <kelvin.vegilla.715@gmail.com>
2024-01-19 13:57:06 +0800

    :sparkles: - Added booking page
    created booking page and form
    used booking api from backend server

--- src/services/apis/dooss/requests/Booking.tsx
+++ src/services/apis/dooss/requests/Booking.tsx
@@ -1,5 +1,9 @@
-import FetchDooss from "../fetch/FetchDooss";
+import FetchDooss, { Booking } from "../fetch/FetchDooss";
 
 export const getBookings = async () => {
   return await FetchDooss("user", "GET");
+};
+
+export const createBooking = async (booking: Booking) => {
+  return await FetchDooss("booking", "POST", booking);
 };
